abdcd068c7c4d27d24c71147eb79d4070f46a5ff
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-06-12 13:35:17 +0100

    Added _ to show to not modify attributes directly
    Added completed attribute to check if completed

--- src/app/todo.js
+++ src/app/todo.js
@@ -1,10 +1,11 @@
 export default class Todo {
     constructor (title, description, dueDate, priority, notes, checked) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.notes = notes;
-        this.checked = checked;
+        this._title = title;
+        this._description = description;
+        this._dueDate = dueDate;
+        this._priority = priority;
+        this._notes = notes;
+        this._checked = checked;
+        this._completed = completed;
     }
 }
