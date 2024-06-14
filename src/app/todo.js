835a8a55949a6e68cb1ecbdf0fdf95994cd01202
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-06-14 09:02:14 +0100

    Added getters and setters for properties
    Removed checked from arguments for the constructor
    
    Set _checked and _completed to false as default for creating a todo

--- src/app/todo.js
+++ src/app/todo.js
@@ -1,14 +1,74 @@
 import generateId from "./id";
 
 export default class Todo {
-    constructor (title, description, dueDate, priority, notes, checked) {
+    constructor (title, description, dueDate, priority, notes) {
         this._title = title;
         this._description = description;
         this._dueDate = dueDate;
         this._priority = priority;
         this._notes = notes;
-        this._checked = checked;
-        this._completed = completed;
+        this._checked = false;
+        this._completed = false;
         this._id = generateId();
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(value) {
+        this._title = value;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    set dueDate(value) {
+        this._dueDate = value;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    set priority(value) {
+        this._priority = value;
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    set notes(value) {
+        this._notes = value;
+    }
+
+    get checked() {
+        return this._checked;
+    }
+
+    set checked(value) {
+        this._checked = value;
+    }
+
+    get completed() {
+        return this._completed;
+    }
+
+    set completed(value) {
+        this._completed = value;
     }
 }
