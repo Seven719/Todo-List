25160f41c00465b7a875883a0b66af4c5a6bf528
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-08-23 12:00:55 +0100

    Refactor
    Separated event listeners from main function for creating todo DOM page
    
    Made function for listeners that match a certain pattern for properties
    
    Separated details event listener into a toggleDetails function
    
    Made create functions for interactive DOM elements

--- src/ui/todo-ui.js
+++ src/ui/todo-ui.js
@@ -25,7 +25,7 @@ const addTodoListener = () => {
         addTodoBtn.removeEventListener("click", handleAddTodo);
         addTodoBtn.addEventListener("click", handleAddTodo);
     }
-};
+}
 
 function handleAddTodo() {
     let tmpTodo = new Todo();
@@ -37,81 +37,53 @@ const createTodoDOM = (todoItem, project) => {
     const todo = document.createElement("button");
 
     const todoMain = document.createElement("div");
-
-    const title = document.createElement("input");
-    const checkbox = createCheckbox(todoItem, title);
-    const details = document.createElement("div");
+    const todoLeft= document.createElement("div");
+    const todoRight = document.createElement("div");
 
     const priority = createPriorityDropdown(todoItem, "high", "medium", "low");
     const description = createDescription(todoItem);
 
-    const date = document.createElement("input");
-    const deleteBtn = document.createElement("img");
+    const title = createTitle(todoItem);
+    const checkbox = createCheckbox(todoItem, title);
+    const details = createDetails(priority, description);
 
-    const todoLeft= document.createElement("div");
-    const todoRight = document.createElement("div");
+    const dueDate = createDueDate(todoItem);
+    const deleteBtn = createDeleteBtn(todo, todoItem, project);
+
+    todo.classList.add("todo");
+    todo.append(todoMain);
 
     todoMain.classList.add("todo-main");
-
-    details.classList.add("todo-details");
-    details.append(priority, description);
+    todoMain.append(todoLeft, todoRight);
 
     todoLeft.classList.add("todo-left");
     todoRight.classList.add("todo-right");
-
-
-
-    title.placeholder = "Enter something...";
-    if (todoItem.title == undefined) {
-        title.value = "";
-    } else {
-        title.value = todoItem.title;
-    }
-
     todoLeft.append(checkbox, title);
+    todoRight.append(dueDate, deleteBtn);
 
-    date.type = "date";
-    date.id = "due-date";
-    date.value = todoItem.dueDate || "";
+    toggleDetails(todo, details);
+    removeOverlappingEventListener(checkbox, title, dueDate, deleteBtn, priority, description);
 
-    deleteBtn.src = Trash;
-    todoRight.append(date, deleteBtn);
-
-    todo.classList.add("todo");
-    todoMain.append(todoLeft, todoRight);
-    todo.append(todoMain);
-
-
-    title.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            title.blur();
-        }
-    });
-
-    title.addEventListener('input', () => {
-        todoItem.title = title.value;
-    });
-
-    date.addEventListener('input', () => {
-        todoItem.dueDate = date.value;
-    });
-
-    description.addEventListener('input', () => {
-        todoItem.description = description.value;
-    });
-
-    todo.addEventListener('click', () => {
-        if (todo.contains(details)) {
-            todo.removeChild(details);
-        } else {
-            todo.appendChild(details);
-        }
-    });
-
-    removeOverlappingEventListener(checkbox, title, date, deleteBtn, priority, description);
-    deleteBtn.addEventListener('click', () => deleteTodo(todo, todoItem, project))
+    manageTodoListeners(todoItem,
+        checkbox,
+        title,
+        dueDate,
+        deleteBtn,
+        priority,
+        description
+    );
 
     return todo;
+}
+
+const createDeleteBtn = (todo, todoItem, project) => {
+    const deleteBtn = document.createElement("img");
+
+    deleteBtn.src = Trash;
+
+    deleteBtn.addEventListener('click', () => deleteTodo(todo, todoItem, project))
+
+    return deleteBtn;
 }
 
 const deleteTodo = (todo, todoItem, project) => {
@@ -143,9 +115,7 @@ const createPriorityDropdown = (todoItem, ...values) => {
         temp.textContent = element.charAt(0).toUpperCase() + element.slice(1);
         priority.appendChild(temp);
 
-        temp.addEventListener('click', () => {
-            todoItem.priority = temp.value;
-        });
+        addListenersForProperties(todoItem, 'click', [temp, 'priority']);
     });
 
     priority.style.width = "10%";
@@ -156,12 +126,10 @@ const createPriorityDropdown = (todoItem, ...values) => {
 const createDescription = (todoItem) => {
     const description = document.createElement("textarea");
 
-    if (todoItem.description == undefined) {
-        description.value = "";
-    } else {
+    description.value = "";
+    if (todoItem.description != undefined) {
         description.value = todoItem.description;
     }
-
     description.placeholder = "Description...";
 
     description.style.resize = "vertical";
@@ -181,6 +149,75 @@ const createCheckbox = (todoItem, title) => {
         title.style.textDecoration = "none";
     }
 
+    return checkbox
+}
+
+const createTitle = (todoItem) => {
+    const title = document.createElement("input");
+
+    title.placeholder = "Enter something...";
+    if (todoItem.title == undefined) {
+        title.value = "";
+    } else {
+        title.value = todoItem.title;
+    }
+
+    return title
+}
+
+const createDueDate = (todoItem) => {
+    const dueDate = document.createElement("input");
+
+    dueDate.type = "date";
+    dueDate.id = "due-date";
+    dueDate.value = todoItem.dueDate || "";
+
+    return dueDate;
+}
+
+const createDetails = (priority, description) => {
+    const details = document.createElement("div");
+
+    details.classList.add("todo-details");
+    details.append(priority, description);
+
+    return details;
+}
+
+const toggleDetails = (todo, details) => {
+    todo.addEventListener('click', () => {
+        if (todo.contains(details)) {
+            todo.removeChild(details);
+        } else {
+            todo.appendChild(details);
+        }
+    });
+}
+
+const addListenersForProperties = (todoItem, eventType, ...entries) => {
+    entries.forEach(([element, property]) => {
+        element.addEventListener(eventType, () => {
+            todoItem[property] = element.value;
+        });
+    });
+}
+
+
+const manageTodoListeners = (todoItem,
+    checkbox,
+    title,
+    dueDate,
+    deleteBtn,
+    priority,
+    description
+) => {
+
+    title.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            title.blur();
+        }
+    });
+
     checkbox.addEventListener('click', () => {
         if (checkbox.checked == true){
             title.style.textDecoration = "line-through";
@@ -190,5 +227,13 @@ const createCheckbox = (todoItem, title) => {
         todoItem.completed = checkbox.checked;
     })
 
-    return checkbox
+    addListenersForProperties(
+        todoItem,
+        'input',
+        [description, 'description'],
+        [dueDate, 'dueDate'],
+        [title, 'title'],
+    );
+
+    removeOverlappingEventListener(checkbox, title, dueDate, deleteBtn, priority, description);
 }
