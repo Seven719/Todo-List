b953f7ba30ed8c5d7cea3176b0d7d2229bff894a
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-08-23 10:07:48 +0100

    Added priority, description and completed properties to Todo Class
    Implemented getters and setters for the newly added properties

--- src/app/todo.js
+++ src/app/todo.js
@@ -1,9 +1,12 @@
 import generateId from "./id";
 
 export default class Todo {
-    constructor (title, dueDate) {
+    constructor (title, description, dueDate, priority) {
         this._title = title;
+        this._description = description;
         this._dueDate = dueDate;
+        this._priority = priority;
+        this._completed = false;
         this._id = generateId();
     }
 
@@ -19,11 +22,35 @@ export default class Todo {
         return this._id;
     }
 
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+
     get dueDate() {
         return this._dueDate;
     }
 
     set dueDate(value) {
         this._dueDate = value;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    set priority(value) {
+        this._priority = value;
+    }
+
+    get completed() {
+        return this._completed;
+    }
+
+    set completed(value) {
+        this._completed = value;
     }
 }
